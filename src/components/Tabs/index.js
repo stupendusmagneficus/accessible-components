cff2356c27f93524e9a868ab51e18acad5d26220
yahor.dunayeu <yahordunayeu@gmail.com>
2022-09-20 08:04:50 +0200

    Initial commit.

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,65 +1,134 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 export default function Tabs() {
-    const data = [
-        {
-            id: 'panel-react',
-            title: 'React',
-            text: 'React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by <a href="https://about.facebook.com/">Meta</a> (formerly Facebook) and a community of individual developers and companies.'
-        },
-        {
-            id: 'panel-vue',
-            title: 'Vue',
-            text: 'Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and <a href="https://en.wikipedia.org/wiki/Single-page_application">single-page applications</a>. It was created by Evan You, and is maintained by him and the rest of the active core team members.'
-        },
-        {
-            id: 'panel-angular',
-            title: 'Angular',
-            text: 'AngularJS is a discontinued free and open-source JavaScript-based web framework for developing single-page applications. It was maintained mainly by <a href="https://www.apple.com/">Google</a> and a community of individuals and corporations.'
-        },
-    ]
+  const data = [
+    {
+      id: 'panel-react',
+      title: 'React',
+      text: 'React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by <a href="https://about.facebook.com/">Meta</a> (formerly Facebook) and a community of individual developers and companies.',
+    },
+    {
+      id: 'panel-vue',
+      title: 'Vue',
+      text: 'Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and <a href="https://en.wikipedia.org/wiki/Single-page_application">single-page applications</a>. It was created by Evan You, and is maintained by him and the rest of the active core team members.',
+    },
+    {
+      id: 'panel-angular',
+      title: 'Angular',
+      text: 'AngularJS is a discontinued free and open-source JavaScript-based web framework for developing single-page applications. It was maintained mainly by <a href="https://www.apple.com/">Google</a> and a community of individuals and corporations.',
+    },
+  ]
 
-    const [activePanelId, setActivePanelId] = useState(data[0].id)
+  const [activePanelId, setActivePanelId] = useState(data[0].id)
 
-    const handleTablistClick = (id) => setActivePanelId(id)
+  const handleTablistClick = (id) => setActivePanelId(id)
 
-    const isPanelActive = (id) => id === activePanelId
+  const isPanelActive = (id) => id === activePanelId
 
-    return (
-        <div className="tabs">
-            <ul className="tabs__list" role="tablist">
-                {data.map(({id, title}) => (
-                    <li
-                        data-tabpanel-id={id}
-                        key={id}
-                        onClick={() => handleTablistClick(id)}
-                        onMouseEnter={() => handleTablistClick(id)}
-                        tabIndex={0}
-                        role="tab"
-                        className={`
+  const handleKeyDown = (e) => {
+    switch (e.key) {
+      case 'ArrowRight':
+        e.preventDefault()
+        changeFocusToItem(e.currentTarget, 'next')
+        break
+
+      case 'ArrowLeft':
+        e.preventDefault()
+        changeFocusToItem(e.currentTarget, 'prev')
+        break
+
+      case 'Home':
+        e.preventDefault()
+        changeFocusToItem(e.currentTarget, 'first')
+        break
+
+      case 'End':
+        e.preventDefault()
+        changeFocusToItem(e.currentTarget, 'last')
+        break
+
+      case 'Enter':
+        handleTablistClick(e.target.dataset.tabpanelId)
+        break
+
+      case 'Space':
+        e.preventDefault()
+        handleTablistClick(e.target.dataset.tabpanelId)
+        break
+
+      default:
+        break
+    }
+  }
+
+  const changeFocusToItem = (target, item) => {
+    const items = Array.from(target.querySelectorAll('.tabs__list-item'))
+
+    let index = [...target.children].indexOf(
+      document.activeElement.closest('.tabs__list'),
+    )
+
+    if (item === 'last') {
+      index = items.length - 1
+    } else if (item === 'first') {
+      index = 0
+    } else if (item === 'next') {
+      index = index === items.length - 1 ? 0 : index + 1
+    } else if (item === 'prev') {
+      index = index <= 0 ? items.length - 1 : index - 1
+    }
+
+    items[index]?.focus()
+  }
+
+  return (
+    <div className="tabs" onKeyDown={handleKeyDown}>
+      <ul className="tabs__list" role="tablist">
+        {data.map(({ id, title }, idx) => (
+          <li
+            aria-selected={isPanelActive(id)}
+            aria-controls={`tabpanel-${idx}`}
+            data-tabpanel-id={id}
+            key={id}
+            id={`tab-${idx}`}
+            onClick={() => handleTablistClick(id)}
+            role="tab"
+            tabIndex={isPanelActive(id) ? 0 : -1}
+            className={`
               tabs__list-item   
               ${isPanelActive(id) ? 'tabs__list-item--active' : ''}
             `}
-                    >{title}</li>
-                ))}
-            </ul>
+          >
+            {title}
+          </li>
+        ))}
+      </ul>
 
-            <p>Front-end web development is the development of the graphical user interface of a website, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that website.</p>
-            <p>There are several libraries and frameworks that help speed up the front end development. The most popular are React, Vue, and Angular.</p>
+      <p>
+        Front-end web development is the development of the graphical user
+        interface of a website, through the use of HTML, CSS, and JavaScript, so
+        that users can view and interact with that website.
+      </p>
+      <p>
+        There are several libraries and frameworks that help speed up the front
+        end development. The most popular are React, Vue, and Angular.
+      </p>
 
-            <div className="tabs__panel-container" role="tabpanel">
-                {data.map(({id, text}) => (
-                    <div
-                        key={id}
-                        id={id}
-                        className={`
+      <div className="tabs__panel-container">
+        {data.map(({ id, text }, idx) => (
+          <div
+            role="tabpanel"
+            aria-labelledby={`tab-${idx}`}
+            key={id}
+            id={`tabpanel-${idx}`}
+            className={`
               tabs__panel
               ${isPanelActive(id) ? 'tabs__panel--active' : ''}
             `}
-                        dangerouslySetInnerHTML={{__html: text}}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+            dangerouslySetInnerHTML={{ __html: text }}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
